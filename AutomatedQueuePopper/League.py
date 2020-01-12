2593bfb070443fe99ff581bd0c224e310afa5cc9
BryanHeYang <59769290+BryanHeYang@users.noreply.github.com>
2020-01-12 10:46:59 -0500

    finished autoclick

--- AutomatedQueuePopper/League.py
+++ AutomatedQueuePopper/League.py
@@ -1,43 +1,17 @@
-import numpy as np
-from PIL import ImageGrab, Image
-import cv2
-import pytesseract
-import math
-
-
-
-class Process:
-    def __init__(self):
-        self.target = "decline"
-        self.ocrErr = False
-
-    def processImg(self, greyImg):
-        txt = pytesseract.image_to_string(greyImg)
-        print(txt)
-        if self.target in txt:
-            print('fu')
-        try:        
-            print('fu')
-        except:
-            print('fu')
-
+import PIL
+import pyautogui 
 
 def main():
-    process = Process()
-    while(True):
-        x = 760
-        y = 968
-
-        offx = 50
-        offy = 22
-        img = ImageGrab.grab(bbox=(x, y, x + offx, y + offy)).convert('L')
-        
-        img = np.array(img)
-        cv2.imshow('window', img)
-        
-        if cv2.waitKey(25) & 0xFF == ord('q'):  
-            cv2.destroyAllWindows()
+   while True:
+        try:
+            box = pyautogui.locateOnScreen("C:/Users/Bryan/Documents/GitHub/Cuhacking/decline.png", confidence = 0.55)
+            loc = pyautogui.center(box)
+            print(loc)
+            pyautogui.click(loc.x, loc.y)
             break
+        except:
+            print("bropkebobuo")
+        
 
 if __name__ == '__main__':
     main()
