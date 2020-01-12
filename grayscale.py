1eeea65a7195d887d6378c2ad65be627cc0af5e1
RogerL <bob.rogerbob1@gmail.com>
2020-01-11 23:15:58 -0500

    Update grayscale.py

--- grayscale.py
+++ grayscale.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[66]:
-
 
 import skimage
 from skimage.color import rgb2gray
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-
-
-# In[63]:
 
 
 grayscale = rgb2gray(skimage.io.imread("./chicken.jpg"))
@@ -23,21 +15,9 @@ ax.set_title("Grayscale")
 plt.show()
 
 
-# In[67]:
 
-
-np.set_printoptions(threshold=sys.maxsize)
-print(grayscale)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
+#np.set_printoptions(threshold=sys.maxsize)
+#print(grayscale)
 
 
 
